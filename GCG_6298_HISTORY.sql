c6fa29b4bbf40e2bb580fb4473ea8b1a57092d04
Service Web <sw@gssmail.com>
2024-03-18 13:41:33 -0500

    Commit changes in C:\Websites\Git\emarlow\6298\GCG_6298_HISTORY.sql

---
+++ GCG_6298_HISTORY.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "GCG_6298_HISTORY" (
+ "HistoryID" CHAR(15) NOT NULL,
+ "AssignID" CHAR(15) NOT NULL,
+ "CourseRev" CHAR(3) NOT NULL,
+ "AssignDate" DATE NOT NULL,
+ "AssignDueDate" DATE NOT NULL,
+ "AssignCompleteDate" DATE NOT NULL,
+ "CompleteBy" CHAR(8) NOT NULL,
+ "CompleteTimeStamp" DATETIME NOT NULL,
+ "Employee" CHAR(5) NOT NULL,
+ "CourseID" CHAR(15) NOT NULL );
